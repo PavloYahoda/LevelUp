8f6702499ff15094d2b8d7ca69161f0bf2c2f303
Pavel <pavlo.yahoda@gmail.com>
2022-02-04 16:47:52 +0200

    test

--- HomeTask1.java
+++ HomeTask1.java
@@ -1,7 +1,7 @@
 public class HomeTask1 {
     public static void main(String[] args) {
         int a=5;
-        int b=2;//test
+        int b=2;//test5555
         int c=a+b;
         System.out.println("a= "+a);
         System.out.println("b= "+b);
