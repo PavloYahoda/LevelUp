e888642047d3f51cc47960687da7a3949c6d4f3d
Pavel <pavlo.yahoda@gmail.com>
2022-02-04 19:54:23 +0200

    Home task 1

--- HomeTask1.java
+++ HomeTask1.java
@@ -1,10 +1,12 @@
 public class HomeTask1 {
     public static void main(String[] args) {
-        int a=5;
-        int b=2;//test12
-        int c=a+b;
-        System.out.println("a= "+a);
-        System.out.println("b= "+b);
-        System.out.println("c= "+c);
-    }
+        double y = 2.71;
+        double Z = 3 * Math.pow(y, 2)+Math.sqrt(( Math.pow(y, 3)) + 1 );
+        double S = Math.sqrt( Math.cos( Math.toRadians(4*Math.pow(y, 2))) + 7.151);
+
+        System.out.println("y= "+y);
+        System.out.printf("Z= %.3f %n", Z);
+        System.out.printf("S= %.3f %n", S);
+
+        }
 }
